dc0a7b30c1eff5d03ebd8c154859b323e852cd00
sunita-tech <60683248+sunita-tech@users.noreply.github.com>
2020-03-20 13:39:05 -0700

    Update yahoo.sql

--- yahoo.sql
+++ yahoo.sql
@@ -1,11 +1,11 @@
-# this sql is creating table in phoenix to upload data from yahoo finance
+# this sql is creating table in phoenix to upload data from API for yahoo finance
 
 drop table if exists yahoo;
 create table if not exists yahoo (
     v_datetime varchar(20),
     v_OPEN decimal(10,2) not null,
     v_HIGH decimal(10,2),
-    v_LOW  decimal(10,2), 
+    v_LOW  decimal(10,2),
     v_CLOSE decimal(10,2),
-    v_VOLUME decimal(10,2),
-	CONSTRAINT pk PRIMARY KEY (v_OPEN));
+    v_VOLUME decimal(10,2)
+        CONSTRAINT pk PRIMARY KEY (v_OPEN));                                        
