4fb9ad4bc090fd8c13c5674cf9f1ca6494a1ef03
sunita-tech <60683248+sunita-tech@users.noreply.github.com>
2020-03-19 23:14:13 -0700

    Update yahoo.sql

--- yahoo.sql
+++ yahoo.sql
@@ -2,10 +2,10 @@
 
 drop table if exists yahoo;
 create table if not exists yahoo (
-    v_datetime TIMESTAMP,
-    v_OPEN varchar(10,2) not null,
-    v_HIGH varchar(10,2),
-    v_LOW  varchar(10,2), 
-    v_CLOSE varchar(10,2),
-    v_VOLUME varchar(10,2),
+    v_datetime varchar(20),
+    v_OPEN decimal(10,2) not null,
+    v_HIGH decimal(10,2),
+    v_LOW  decimal(10,2), 
+    v_CLOSE decimal(10,2),
+    v_VOLUME decimal(10,2),
 	CONSTRAINT pk PRIMARY KEY (v_OPEN));
